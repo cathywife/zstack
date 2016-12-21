b419ac60bd5f52fe05abbd08adc85839e4168ae5
miao.DengSheng <xuexuemiao@yeah.net>
2016-12-21 17:16:38 +0800

    working on shareable volume

--- test/src/test/java/org/zstack/test/storage/volume/TestCreateShareableDataVolume.java
+++ test/src/test/java/org/zstack/test/storage/volume/TestCreateShareableDataVolume.java
@@ -9,13 +9,16 @@ import org.zstack.core.db.DatabaseFacade;
 import org.zstack.core.db.SimpleQuery;
 import org.zstack.header.configuration.DiskOfferingInventory;
 import org.zstack.header.identity.SessionInventory;
+import org.zstack.header.query.QueryOp;
 import org.zstack.header.storage.primary.PrimaryStorageInventory;
 import org.zstack.header.vm.VmInstanceInventory;
 import org.zstack.header.volume.APICreateDataVolumeEvent;
 import org.zstack.header.volume.APICreateDataVolumeMsg;
 import org.zstack.header.volume.VolumeInventory;
+import org.zstack.mevoco.APIQueryShareableVolumeVmInstanceRefMsg;
 import org.zstack.mevoco.MevocoSystemTags;
 import org.zstack.mevoco.ShareableVolumeVmInstanceRefVO;
+import org.zstack.mevoco.ShareableVolumeVmInstanceRefVO_;
 import org.zstack.test.Api;
 import org.zstack.test.ApiSender;
 import org.zstack.test.ApiSenderException;
@@ -53,30 +56,53 @@ public class TestCreateShareableDataVolume {
 
         ApiSender sender = new ApiSender();
         sender.setTimeout(1200);
-        APICreateDataVolumeMsg msg = new APICreateDataVolumeMsg();
-        msg.setSession(session);
-        msg.setPrimaryStorageUuid(ps1.getUuid());
-        msg.setName("shareable volume");
-        msg.setDiskOfferingUuid(diskOfferingInventory.getUuid());
-        String tag = MevocoSystemTags.SHAREABLE_DEVICE.getTagFormat();
-        msg.setSystemTags(Arrays.asList(tag));
-        APICreateDataVolumeEvent e = sender.send(msg, APICreateDataVolumeEvent.class);
-        VolumeInventory vol = e.getInventory();
 
-        Assert.assertTrue(vol.isShareable());
+        VolumeInventory vol;
+        {
+            APICreateDataVolumeMsg msg = new APICreateDataVolumeMsg();
+            msg.setSession(session);
+            msg.setPrimaryStorageUuid(ps1.getUuid());
+            msg.setName("shareable volume");
+            msg.setDiskOfferingUuid(diskOfferingInventory.getUuid());
+            String tag = MevocoSystemTags.SHAREABLE_DEVICE.getTagFormat();
+            msg.setSystemTags(Arrays.asList(tag));
+            APICreateDataVolumeEvent e = sender.send(msg, APICreateDataVolumeEvent.class);
+            vol = e.getInventory();
+
+            Assert.assertTrue(vol.isShareable());
+        }
+
+        {
+            APIQueryShareableVolumeVmInstanceRefMsg msg = new APIQueryShareableVolumeVmInstanceRefMsg();
+            msg.setSession(session);
+            msg.addQueryCondition(ShareableVolumeVmInstanceRefVO_.volumeUuid.getName(), QueryOp.EQ, vol.getUuid());
+            Assert.assertTrue(0 == api.queryCount(msg, session));
+        }
 
         api.attachVolumeToVm(vm.getUuid(), vol.getUuid());
-
         {
             SimpleQuery<ShareableVolumeVmInstanceRefVO> q = dbf.createQuery(ShareableVolumeVmInstanceRefVO.class);
             Assert.assertTrue(q.count() == 1);
         }
 
-        api.detachVolumeFromVmEx(vol.getUuid(), vm.getUuid(), null);
+        {
+            APIQueryShareableVolumeVmInstanceRefMsg msg = new APIQueryShareableVolumeVmInstanceRefMsg();
+            msg.setSession(session);
+            msg.addQueryCondition(ShareableVolumeVmInstanceRefVO_.volumeUuid.getName(), QueryOp.EQ, vol.getUuid());
+            Assert.assertTrue(1 == api.queryCount(msg, session));
+        }
 
+        api.detachVolumeFromVmEx(vol.getUuid(), vm.getUuid(), null);
         {
             SimpleQuery<ShareableVolumeVmInstanceRefVO> q = dbf.createQuery(ShareableVolumeVmInstanceRefVO.class);
             Assert.assertTrue(q.count() == 0);
+        }
+
+        {
+            APIQueryShareableVolumeVmInstanceRefMsg msg = new APIQueryShareableVolumeVmInstanceRefMsg();
+            msg.setSession(session);
+            msg.addQueryCondition(ShareableVolumeVmInstanceRefVO_.volumeUuid.getName(), QueryOp.EQ, vol.getUuid());
+            Assert.assertTrue(0 == api.queryCount(msg, session));
         }
     }
 }
